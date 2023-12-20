1727fbfbc2f803820792888099c83e82cad7ee7e
Dmitry Lorents <zmey.task@gmail.com>
2023-12-20 20:57:22 +0300

    Add ScrollView

--- HotelApp/Modules/HotelModule/View/HotelView.swift
+++ HotelApp/Modules/HotelModule/View/HotelView.swift
@@ -1,21 +1,44 @@
-//
-//  HotelView.swift
-//  HotelApp
-//
-//  Created by Dmitry on 20.12.2023.
-//
+
 
 import UIKit
+import SnapKit
 
 class HotelView: UIView {
 
     //MARK: - Parameters
+    
+    private lazy var scrollView: UIScrollView = {
+        let view = UIScrollView()
+        view.showsVerticalScrollIndicator = false
+        view.backgroundColor = .yellow//HotelModel.backgroundColor
+        return view
+    }()
+    
+    private let contentView = UIView()
     
     private let firstView: UIView = {
         let view = UIView()
         view.backgroundColor = .white
         view.layer.cornerRadius = HotelModel.viewCornerRadius
         return view
+    }()
+    
+    private let secondView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.layer.cornerRadius = HotelModel.viewCornerRadius
+        return view
+    }()
+    private let thirdView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        return view
+    }()
+    
+    private lazy var numberChoosingButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle(HotelModel.buttonTitle, for: .normal)
+        return button
     }()
     
     private lazy var topCollectionView: UICollectionView = {
@@ -37,13 +60,55 @@ class HotelView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        print(#function)
+        scrollView.contentSize = CGSize(width: 400, height: 1500)//
+        print(contentView.bounds.size)
+    }
+    
     //MARK: - Methods
     private func setupViews() {
-        backgroundColor = HotelModel.backgroundColor
-        addSubview(firstView)
+        //backgroundColor = HotelModel.backgroundColor
+        addSubview(scrollView)
+        contentView.backgroundColor = .blue
+        scrollView.addSubview(contentView)
+        contentView.addSubview(firstView)
+        contentView.addSubview(secondView)
+        addSubview(thirdView)
+        thirdView.addSubview(numberChoosingButton)
     }
     
     private func setupConstraints() {
+        
+        scrollView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview()
+            make.bottom.equalToSuperview().inset(HotelModel.bottomViewHeight)
+        }
+        contentView.snp.makeConstraints { make in
+            make.leading.trailing.equalTo(self)
+            make.height.equalTo(800)
+        }
+        firstView.snp.makeConstraints { make in
+            make.leading.trailing.top.equalToSuperview()
+            //TODO - delete later
+            make.height.equalTo(530)
+        }
+        
+        secondView.snp.makeConstraints { make in
+            make.top.equalTo(firstView.snp.bottom).inset(-8)
+            make.leading.trailing.height.equalTo(firstView)
+            make.bottom.equalTo(contentView).inset(8)
+        }
+        
+        thirdView.snp.makeConstraints { make in
+            make.leading.trailing.bottom.equalToSuperview()
+            make.top.equalTo(scrollView.snp.bottom)
+        }
+        
+        numberChoosingButton.snp.makeConstraints { make in
+            make.edges.equalToSuperview().inset(UIEdgeInsets(top: 12, left: 16, bottom: 28, right: 16))
+        }
         
     }
 }
