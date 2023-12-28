5fe4c97d52bccebfee438f527d67f12abf3361bc
Dmitry Lorents <zmey.task@gmail.com>
2023-12-28 22:57:19 +0300

    Add accessory type imageView

--- HotelApp/Modules/HotelModule/View/Cells/ButtonsTableViewCell.swift
+++ HotelApp/Modules/HotelModule/View/Cells/ButtonsTableViewCell.swift
@@ -14,8 +14,8 @@ class ButtonsTableViewCell: UITableViewCell {
     
     private var titleLabel = UILabel.makeCellsubtitleLabel()
     private var subtitleLabel = UILabel.makeCellTitleLabel()
-    private var imageViewCell = UIImageView()
-    
+    private var leftImageView = UIImageView()
+    private var rightImageView = UIImageView()
    
     
     //MARK: - Init
@@ -38,27 +38,36 @@ class ButtonsTableViewCell: UITableViewCell {
     public func setCell(with model: (title: String, subtitle: String, imageName: String)) {
         titleLabel.text = model.title
         subtitleLabel.text = model.subtitle
-        imageViewCell.image = UIImage(named: model.imageName)
+        leftImageView.image = UIImage(named: model.imageName)
     }
    
     private func setupViews() {
         //backgroundColor = .cyan//.clear
         contentView.backgroundColor = HotelModel.tableViewBackgroundColor
-        contentView.addSubviews(titleLabel, subtitleLabel, imageViewCell)
+        rightImageView.image = UIImage(systemName: "chevron.right")
+        rightImageView.tintColor = .black
+        rightImageView.contentMode = .center
+        contentView.addSubviews(titleLabel, subtitleLabel, leftImageView, rightImageView)
     }
 }
 //MARK: - Set constraints
 extension ButtonsTableViewCell {
     private func setupConstraints() {
         
-        imageViewCell.snp.makeConstraints { make in
+        leftImageView.snp.makeConstraints { make in
             make.centerY.equalToSuperview()
             make.leading.equalToSuperview().inset(15)
             make.height.width.equalTo(24)
         }
         
+        rightImageView.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.trailing.equalToSuperview().inset(24)
+            make.height.width.equalTo(12)
+        }
+        
         titleLabel.snp.makeConstraints { make in
-            make.leading.equalTo(imageViewCell.snp.trailing).inset(-7)
+            make.leading.equalTo(leftImageView.snp.trailing).inset(-7)
             make.top.equalToSuperview().inset(15)
         }
         
